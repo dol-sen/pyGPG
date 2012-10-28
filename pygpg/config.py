f993b15a457c6fcc37541859f48a26d68efd96aa
Brian Dolbec <dolsen@gentoo.org>
2012-10-28 16:13:52 -0700

    add more to config

--- pygpg/config.py
+++ pygpg/config.py
@@ -24,14 +24,40 @@ class GPGConfig(object):
     for running the GnuPG class'''
 
     def __init__(self):
-        defaults = {
+        self._defaults = {
             'gpg_command': '/usr/bin/gpg',
             'decrypt': '--decrypt',
             'verify': '--verify',
             'sign': '--sign',
             'clearsign': '--clearsign',
             'detach-sign': '--detach-sign',
+            'dump-otions': '--dump-options',
+            'no-tty': '--no-tty',
+            'version': '--version',
         }
+        self._options = ['--no-tty',
+        ]
+
+
+    def get_options(self):
+        '''Returns the list of default options to be applied to
+        all gpg runs except the GnuPG.custom_run()
+        '''
+        return self._options.copy()
+
+
+    def get_defaults(self):
+        '''Returns a dictionary of the default settings
+        '''
+        return self._defaults.copy()
+
+
+    def set_options(self, options):
+        '''Sets the options to the value passed in
+        @param options: list
+        '''
+        self._options = options.copy()
+
 
     def sign_modes(self, gpg_options=None):
         '''Returns the list of supported signing types
