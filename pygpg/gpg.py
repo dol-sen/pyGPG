2cfde9700a5871f407354900bca91eedc3e31e95
Brian Dolbec <dolsen@gentoo.org>
2012-10-28 22:28:42 -0700

    simplify some of the naming to avoid subtle mispellings, initial debugging.

--- pygpg/gpg.py
+++ pygpg/gpg.py
@@ -20,10 +20,12 @@ from __future__ import with_statement
 
 from subprocess import Popen, PIPE
 
+#import pygpg
+#from pygpg import output
 from pygpg.output import GPGResult
 
 
-class GnuPG(object):
+class GPG(object):
     '''Subprocess gnupg handler class'''
 
     def __init__(self, config):
@@ -32,24 +34,28 @@ class GnuPG(object):
         '''
         self.config = config
         self._gpg_version = None
+        self._gpg_options = None
 
 
-    def _process_gpg(self, action, gpg_input, filepath):
+    def _process_gpg(self, action, gpg_input=None, filepath=None):
         '''Creates and opens the subprocess object
         @rtype GnuPGResult object
         '''
         results = ('', '') # null
-        if gpg_input:
+        gpg = None
+        if gpg_input is not None:
             args = [self.config['gpg_command'],
                     self.config[action],
-                    self.config.get_options()
-                ]
+                    self.config['gpg_defaults']]
+            if self.config.task_options[action]:
+                args.append(self.config.task_options[action])
+            print "running gpg with: ", args
             gpg = Popen(args, shell=False, stdin=PIPE,
                 stdout=PIPE, stderr=PIPE)
             results = gpg.communicate(gpg_input)
-        elif filepath:
+        elif filepath is not None:
             pass
-        return GnuPGResult(results)
+        return GPGResult(gpg, results)
 
     def decrypt(self, gpg_input=None, filepath=None):
         '''Decrypts the gpg_input block passed in
@@ -59,34 +65,36 @@ class GnuPG(object):
         '''
         return self._process_gpg('decrypt', gpg_input, filepath)
 
-    def verify(self, gpg_input=None, filename=None):
+    def verify(self, gpg_input=None, filepath=None):
         return self._process_gpg('verify', gpg_input, filepath)
 
     def sign(self, gpg_input=None, filename=None, mode=None):
         if not mode:
-            return GnuPGResult('', 'pyGPG: Error, no mode signing passed in\n')
+            return GPGResult(None, '', 'pyGPG: Error, no mode signing passed in\n')
         return self._process_gpg(mode, gpg_input, filepath)
 
-    def dump_options(self, only_usable=False, refetch=False):
+    @property
+    def dump_options(self):
         '''Runs 'gpg --dump-options'
         @param only_usable: Boolean, When True will filter
             the returned list to only contain the options this class
             is capable of using.
         @param refetch: Boolean
         @rtype list: of options from gpg'''
-        if not self._gpg_options or refetch:
-            self._gpg_options = self._process_gpg('dump-options')
+        if not self._gpg_options or self.config['refetch']:
+            self._gpg_options = self._process_gpg('dump-options', '')
         opts = self._gpg_options.output.split("\n")
-        if only_usable:
+        if self.config['only_usable']:
             return list(set(opts.difference(self.config.unsupported)))
         return opts
 
-    def version(self, refetch=False):
+    @property
+    def version(self):
         '''Runs 'gpg --version'
         @param refetch: Boolean
         @rtype list: of options from gpg'''
-        if not self._gpg_version or refetch:
-            self._gpg_version = self._process_gpg('version')
+        if (self._gpg_version is None) or self.config['refetch']:
+            self._gpg_version = self._process_gpg('version', '')
         return self._gpg_version.output.split("\n")
 
     def custom_run(self, **kwargs):
