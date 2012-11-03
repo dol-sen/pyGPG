075a407829a0e2cc3bf1a1e997a840e68ffd257e
Brian Dolbec <dolsen@gentoo.org>
2012-11-02 17:30:26 -0700

    fill out some primary data functions

--- pygpg/output.py
+++ pygpg/output.py
@@ -21,6 +21,13 @@
 from pygpg.status import Status
 
 
+def encode(text, enc="UTF-8"):
+    """py2, py3 compatibility function"""
+    if hasattr(text, 'decode'):
+        return text.decode(enc)
+    return str(text)
+
+
 class GPGResult(object):
     '''GnuPG process result handler'''
 
@@ -28,37 +35,102 @@ class GPGResult(object):
         self.gpg = gpg
         self.output = results[0]
         self.stderr_out = results[1].split('\n')
-        self._time = None
-        self._signature = None
-        self._key_id = None
-        self._key_type = None
-        self._fingerprint = None
         self.status = Status()
         self.messages = self.status.extract_data(self.stderr_out)
 
 
     @property
     def verified(self):
-        pass
+        good = ['GOODSIG', 'VALIDSIG', 'SIG_CREATED']
+        trusts = ['TRUST_UNDEFINED', 'TRUST_NEVER', 'TRUST_MARGINAL',
+            'TRUST_FULLY', 'TRUST_ULTIMATE']
+        valid = self.get_data(status_type=good) != []
+        trust = self.get_data(status_type=trusts)
+        if len(trust):
+            trust = trust[0]
+        else:
+            trust = 'Unknown'
+        return (valid, trust)
+
 
     @property
     def time(self):
-        pass
+        fields = ['timestamp', 'sig_timestamp', 'expire_timestamp', ]
+        return self.get_data(fields)
+
 
     @property
-    def key_id(self):
-        pass
+    def keyid(self):
+        fields = ['long_keyid', 'long_main_keyid']
+        return self.get_data(fields)
+
 
     @property
-    def key_type(self):
-        pass
+    def keytype(self):
+        fields = ['keytype']
+        return self.get_data(fields)
+
 
     @property
-    def signature(self):
-        pass
+    def fingerprint(self):
+        fields = ['fingerprint']
+        return self.get_data(fields)
+
 
     @property
     def returncode(self):
+        '''The return code of the gpg process that was run '''
         return self.gpg.returncode
 
 
+    def get_fields(self, status_type=None):
+        '''get a list of (type name,fields) tuples available from the gpg run
+
+        @param staus_type: string or list of class name strings
+            to search the data for
+        @returns list: of fields with availble data
+        '''
+        if status_type is None:
+            return [(x.name, x._fields) for x in self.status.data]
+        status_type = self._check_param_type(status_type)
+        return [(x.name, x._fields)
+            for x in self.status.data if x.name in status_type]
+
+
+    def get_data(self, fields=None, status_type=None):
+        '''get a list of (type name,field,data) tuples available from the gpg run
+
+        @param fields: string or list of field name strings to search for
+        @param staus_type: string or list of class name strings to search for
+        @returns list: of (type name,field,data) tuples
+        '''
+        if status_type is None:
+            status_type = [x.name for x in self.status.data]
+        else:
+            status_type = self._check_param_type(status_type)
+        if fields is None:
+            fields = []
+            for x in self.status.data:
+                fields.extend(list(x._fields))
+        else:
+            fields = self._check_param_type(fields)
+
+        results = []
+        for x in self.status.data:
+            if x.name in status_type:
+                for f in x._fields:
+                    if f in fields:
+                        results.append((x.name, f, getattr(x, f)))
+        return results
+
+
+    @staticmethod
+    def _check_param_type(param):
+        """internal function that validates the repos parameter,
+        converting a string to a list[string] if it is not already a list.
+        produces and error message if it is any other type
+        returns repos as list always"""
+        if isinstance(param, basestring):
+            param = [param]
+        # else assume it is an iterable, if not it will error
+        return [encode(i) for i in param]
