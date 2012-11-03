7eb6d59789541aa003858cf4475a68b6f5308fa5
Brian Dolbec <dolsen@gentoo.org>
2012-11-03 13:02:12 -0700

    add version.py

---
+++ pygpg/version.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#################################################################################
+# pyGPG Config
+#################################################################################
+# File:       config.py
+#
+#             Python configuration and options class for
+#             an Interface access to gnupg
+#
+# Copyright:
+#             (c) 2012 Brian Dolbec
+#             Distributed under the terms of the GNU General Public License v2
+#
+# Author(s):
+#             Brian Dolbec <dolsen@gentoo.org>
+#
+
+version = 'git'
+
+License = 'GPL2'
