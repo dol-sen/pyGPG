b6967213cc818cc8e3511b53d392e1e942ed98d3
Brian Dolbec <dolsen@gentoo.org>
2012-11-17 18:37:25 -0800

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys
+
+from distutils.core import setup
+
+# this affects the names of all the directories we do stuff with
+sys.path.insert(0, './')
+
+from layman.version import Version, License
+
+
+__version__ = os.getenv('VERSION', default='9999')
+
+
+setup(name          = 'pygpg',
+      version       = Version,
+      description   = "A python interface wrapper for gnupg's gpg command",
+      author        = 'Brian Dolbec',
+      author_email  = 'dolsen@gentoo',
+      url           = ="https://github.com/dol-sen/pyGPG",
+      packages      = ['pygpg'],
+      license       = License,
+      )
+
