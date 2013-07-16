2ca9c73849238f8ba97ff360e4a771f1a4c2e76f
Brian Dolbec <dolsen@gentoo.org>
2013-07-16 05:04:40 -0700

    Update setup.py for new namespace spelling.
    Fix author email.

--- setup.py
+++ setup.py
@@ -13,13 +13,13 @@ from pygpg.version import Version, License
 #__version__ = os.getenv('VERSION', default='9999')
 
 
-setup(name          = 'pygpg',
+setup(name          = 'pyGPG',
       version       = Version,
       description   = "A python interface wrapper for gnupg's gpg command",
       author        = 'Brian Dolbec',
-      author_email  = 'dolsen@gentoo',
+      author_email  = 'dolsen@gentoo.org',
       url           = "https://github.com/dol-sen/pyGPG",
-      packages      = ['pygpg'],
+      packages      = ['pyGPG'],
       license       = License,
       )
 
