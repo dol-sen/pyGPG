d914a3b39ebe4a5c598924404653d076ca8cabfe
Pavlos Ratis <dastergon@gentoo.org>
2013-12-27 01:44:28 +0200

    minor improvements in setup.py
    * add classifiers
    * add long_description

--- setup.py
+++ setup.py
@@ -3,23 +3,31 @@
 import sys
 
 from distutils.core import setup
+from pyGPG.version import Version, License
 
 # this affects the names of all the directories we do stuff with
 sys.path.insert(0, './')
 
-from pyGPG.version import Version, License
-
-
 #__version__ = os.getenv('VERSION', default='9999')
 
-
-setup(name          = 'pyGPG',
-      version       = Version,
-      description   = "A python interface wrapper for gnupg's gpg command",
-      author        = 'Brian Dolbec',
-      author_email  = 'dolsen@gentoo.org',
-      url           = "https://github.com/dol-sen/pyGPG",
-      packages      = ['pyGPG'],
-      license       = License,
-      )
+setup(
+    name          = 'pyGPG',
+    version       = Version,
+    description   = "A Python interface wrapper for GnuPG's gpg command",
+    author        = 'Brian Dolbec',
+    author_email  = 'dolsen@gentoo.org',
+    url           = "https://github.com/dol-sen/pyGPG",
+    packages      = ['pyGPG'],
+    license       = License,
+    long_description = open('README').read(),
+    keywords = 'gpg',
+    classifiers   = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 2.7',
+        'Operating System :: OS Independent',
+        'Topic :: Security :: Cryptography',
+    ],
+)
 
